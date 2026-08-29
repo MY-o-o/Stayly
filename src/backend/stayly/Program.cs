2329df47e6f9fdb2ea9c2edd6be0eceabda66753
Arsenii <arseniydorofienko@gmail.com>
2026-08-29 12:06:27 +0200

    Fix CORS Policy
    - Allow Credentials in CORS policy

--- src/backend/stayly/Program.cs
+++ src/backend/stayly/Program.cs
@@ -51,7 +51,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins("http://localhost:3000")
             .AllowAnyHeader()
-            .AllowAnyMethod();
+            .AllowAnyMethod()
+            .AllowCredentials();
     });
 });
 
